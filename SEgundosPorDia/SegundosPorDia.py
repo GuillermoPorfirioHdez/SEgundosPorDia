970041277369c1e95f5ea5ee104d456f0bb3f644
GuillermoPorfirioHdez <121981087+GuillermoPorfirioHdez@users.noreply.github.com>
2023-01-04 20:15:44 -0600

    Add files via upload

---
+++ SEgundosPorDia/SegundosPorDia.py
@@ -0,0 +1,12 @@
+#contruir un programa que calcule he imprima el numero de sugundos que hay en un determinado numero de dias.
+
+#Dia = 24 horas
+#Hora = 60 minutos
+#Minutos = 60 segundos
+
+# Cantidad de segunds en un dia es: 86400
+
+print("***¿Cuantos segundos hay en un dia?***")
+dia= int(input("ingrese la cantidad de dias a calcular:"))
+seg=dia*86400
+print("Hay",seg,"segundos en",dia,"dias.")
